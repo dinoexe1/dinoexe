e8c7c1c59f0db6beed51c1ee043ebbc119d33325
dinoexe1 <94439187+dinoexe1@users.noreply.github.com>
2021-11-17 19:23:34 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const Config = require('../config')
 Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by kichu
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Amalser [OWNER]\n' // full name
+            + 'FN:Kichu [OWNER]\n' // full name
             + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid=994408993612:+994408993612\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "Kichu [OWNER]", vcard: vcard}, MessageType.contact)
 }))
