d6e2c8ff397a944203b7584d3fa6ce426633a9fc
dinoexe1 <94439187+dinoexe1@users.noreply.github.com>
2021-11-17 19:33:38 +0530

    Update amalser2.js

--- plugins/amalser2.js
+++ plugins/amalser2.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Amalser - Amal,farhan
+DINO EXE BOT ❌ - Dino,Kichu
 */
 
 const Asena = require('../events');
