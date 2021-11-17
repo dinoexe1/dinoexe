667667ef2b174da334da8a6a15748283a400fdaa
dinoexe1 <94439187+dinoexe1@users.noreply.github.com>
2021-11-17 13:05:57 +0530

    Update Pinky.js

--- Pinky.js
+++ Pinky.js
@@ -1,4 +1,4 @@
-/* codded by afnanplk
+/* codded by dinoexe1
 */
 
 const { Sequelize } = require('sequelize');
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+919895828468' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amalser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+994408993612' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'DINO BOT ❌' : process.env.DEPLOYER,    
 };
